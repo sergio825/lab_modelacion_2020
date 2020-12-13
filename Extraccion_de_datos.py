93fa9b574efaf864490f5251c50ae183e608da4b
sergio825 <72813134+sergio825@users.noreply.github.com>
2020-12-12 22:36:42 -0300

    Add files via upload

---
+++ Extraccion_de_datos.py
@@ -0,0 +1,9 @@
+
+import pandas as pd
+
+ots = pd.read_csv("data/ot.csv",sep=',')
+ots=ots.sort_values('Pedido')
+xlsx_file = "data/layout.xlsx"
+layout = pd.read_excel(xlsx_file, sheet_name="layout")
+adyacencia=pd.read_excel(xlsx_file, sheet_name="adyacencia")
+cant_ordenes=len(ots['Pedido'].unique())
