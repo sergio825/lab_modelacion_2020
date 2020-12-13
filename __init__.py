fa2e8cff98d6db2ea1b8c70bbb984132f59b27e2
sergio825 <72813134+sergio825@users.noreply.github.com>
2020-12-12 23:06:43 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,7 +2,23 @@ import numpy as np
 import pandas as pd
 
 cant_ordenes=len(ots['Pedido'].unique())
+pasillos=layout['pasillo'].unique() 
+
 for x in range(cant_ordenes): 
   obj=list(ots.loc[ots["Pedido"]==x+1]["Cod.Prod"])
   lista.append(obj)
 ordenes=np.array(lista)
+
+l=[]
+for pasillo in pasillos:
+  l.append([pasillo,False, 0])
+pasillo_bool=pd.DataFrame(l,columns=["pasillo","ocupado","pickeador"])
+
+pasillo_act=[]
+for x in range(cant_pickeadores): 
+  pasillo_act.append(0)
+ 
+pickeadores=[]
+for x in range(cant_pickeadores):
+  pickeadores.append(0)
+
