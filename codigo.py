c9fbbc770d19789b1a92f83752a777b31336f72f
sergio825 <72813134+sergio825@users.noreply.github.com>
2020-12-15 12:08:01 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,5 +1,3 @@
-
-
 def asignar_ordenes(pickeadores:list, ordenes_enum:list, cant_pickeadores:int )->list:
     for x in range(cant_pickeadores):
         if pickeadores[x]==0:
@@ -11,18 +9,19 @@ def asignar_ordenes(pickeadores:list, ordenes_enum:list, cant_pickeadores:int )-
 def next_move(pasillo_a_ir:float,pasillo_act:float, adyacencia:pd.core.frame.DataFrame)->float:
     ady=adyacencia.loc[adyacencia["pasillo"]==pasillo_act]
     for x in range(ady.shape[0]):
-        if existe_ruta(pasillo_a_ir,ady.iloc[x,1]):
+        if existe_ruta(pasillo_a_ir,ady.iloc[x,1],adyacencia):
             return ady.iloc[x,1]
-            
+        
+        
 def ordenar(orden:list, pasillos:np.ndarray):
     lista_ordenada=[]
     for pasillo in pasillos:
         for x in range(len(orden)):
-            if pasillo==objeto_en_pasillo(orden[x]):
+            if pasillo==objeto_en_pasillo(orden[x],layout):
                 lista_ordenada.append(orden[x])
     return lista_ordenada
-                 
-            
+
+
 def existe_ruta(p1:float,p2:float, adyacencia:pd.core.frame.DataFrame)->bool:
     ady=adyacencia.loc[adyacencia["pasillo"]==p2]
     
@@ -36,13 +35,13 @@ def existe_ruta(p1:float,p2:float, adyacencia:pd.core.frame.DataFrame)->bool:
             if ady.iloc[x,1]==-1:
                 return False
             else:
-                return existe_ruta(p1,ady.iloc[x,1])
-            
+                return existe_ruta(p1,ady.iloc[x,1],adyacencia)
+
 def ruta(pasillo_a_ir:float,pasillo_act:float)->list:
     x=pasillo_act
     l=[]
     while x!=pasillo_a_ir:
-        x=next_move(pasillo_a_ir,x)
+        x=next_move(pasillo_a_ir,x,adyacencia)
         l.append(x)
     return l
 
@@ -54,6 +53,10 @@ def objeto_en_pasillo(cod_prod:float, layout:pd.core.frame.DataFrame):
 def ocupado(pasillo:float,pasillo_bool:pd.core.frame.DataFrame)->bool:
     p=pasillo_bool.loc[pasillo_bool["pasillo"]==pasillo]
     return p.iloc[0,1]
+
+
+
+
 
 def tiempo(ordenes:np.ndarray,cant_pickeadores:int, ots:pd.core.frame.DataFrame,
            layout:pd.core.frame.DataFrame, adyacencia:pd.core.frame.DataFrame):
@@ -68,11 +71,14 @@ def tiempo(ordenes:np.ndarray,cant_pickeadores:int, ots:pd.core.frame.DataFrame,
     ordenes_enum=ots["Pedido"].unique()
     ordenes_enum=list(ordenes_enum)
     pasillo_act=[]
-    pasillos=layout['pasillo'].unique() 
+    pasillos=layout['pasillo'].unique()
     
-    for x in range(len(ordenes)):
-        ordenes[x]=ordenar(ordenes[x])
-        
+    for x in range(cant_pickeadores): 
+        pasillo_act.append(0)
+ 
+    pickeadores=[]
+    for x in range(cant_pickeadores):
+        pickeadores.append(0)    
     for x in range(cant_pickeadores):
         to_end.append(0)
         
@@ -81,9 +87,9 @@ def tiempo(ordenes:np.ndarray,cant_pickeadores:int, ots:pd.core.frame.DataFrame,
         
     while len(ordenes_enum)>=0:
         
-        if asignar_ordenes(pickeadores)==to_end:
+        if asignar_ordenes(pickeadores,ordenes_enum,cant_pickeadores)==to_end:
             break
-        asignar_ordenes(pickeadores)
+        asignar_ordenes(pickeadores,ordenes_enum,cant_pickeadores)
         for x in range(cant_pickeadores):
             
             if pickeadores[x]!=0: 
@@ -95,7 +101,7 @@ def tiempo(ordenes:np.ndarray,cant_pickeadores:int, ots:pd.core.frame.DataFrame,
                     next_move=-1
                 else:
                     print("Siguiente producto a recoger:", ordenes[pickeadores[x]-1][contador[x]])
-                    next_move=objeto_en_pasillo(ordenes[pickeadores[x]-1][contador[x]])
+                    next_move=objeto_en_pasillo(ordenes[pickeadores[x]-1][contador[x]],layout)
                 if len(ruta(next_move,pasillo_act[x]))==0:#estoy en el pasillo a recoger producto
                     if pasillo_act[x]==-1:
                         print("pickeador", "termino orden ", pickeadores[x] ,"al minuto:", t/60 )
@@ -127,7 +133,7 @@ def tiempo(ordenes:np.ndarray,cant_pickeadores:int, ots:pd.core.frame.DataFrame,
                     
                     else:
                         print("siguiente pasillo a visitar:",route[0])
-                        if ocupado(route[0]):
+                        if ocupado(route[0],pasillo_bool):
                             print("pasillo",route[0], " ocupado al minuto:", t/60)
                             t_wait+=tiempo_pickeo
                             t+=tiempo_pickeo
@@ -156,3 +162,4 @@ def tiempo(ordenes:np.ndarray,cant_pickeadores:int, ots:pd.core.frame.DataFrame,
     print("tiempo de pickeo:", t_pick/60)
     
     return t/60
+ 
