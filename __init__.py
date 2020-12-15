ec937b82344823aa469771dbd7c314562855b49c
sergio825 <72813134+sergio825@users.noreply.github.com>
2020-12-15 12:03:24 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,24 +1,18 @@
-import numpy as np
-import pandas as pd
+#Algunas Consideraciones:
+tiempo_pickeo=20#[s]
+velocidad=20# [m/s]
 
-cant_ordenes=len(ots['Pedido'].unique())
-pasillos=layout['pasillo'].unique() 
-
+lista=[]
 for x in range(cant_ordenes): 
   obj=list(ots.loc[ots["Pedido"]==x+1]["Cod.Prod"])
   lista.append(obj)
 ordenes=np.array(lista)
 
+for x in range(len(ordenes)):
+  ordenes[x]=ordenar(ordenes[x],pasillos)
+
+
 l=[]
 for pasillo in pasillos:
   l.append([pasillo,False, 0])
 pasillo_bool=pd.DataFrame(l,columns=["pasillo","ocupado","pickeador"])
-
-pasillo_act=[]
-for x in range(cant_pickeadores): 
-  pasillo_act.append(0)
- 
-pickeadores=[]
-for x in range(cant_pickeadores):
-  pickeadores.append(0)
-
