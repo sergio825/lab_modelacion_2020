e1adb802aca495df13f8ae1581ecac2f1b90069b
sergio825 <72813134+sergio825@users.noreply.github.com>
2020-12-15 11:59:00 -0300

    Update Extraccion_de_datos.py
    Extraccion de ordenes, layout,etc.

--- Extraccion_de_datos.py
+++ Extraccion_de_datos.py
@@ -1,5 +1,5 @@
-
 import pandas as pd
+import numpy as np
 
 ots = pd.read_csv("data/ot.csv",sep=',')
 ots=ots.sort_values('Pedido')
@@ -7,3 +7,4 @@ xlsx_file = "data/layout.xlsx"
 layout = pd.read_excel(xlsx_file, sheet_name="layout")
 adyacencia=pd.read_excel(xlsx_file, sheet_name="adyacencia")
 cant_ordenes=len(ots['Pedido'].unique())
+pasillos=layout['pasillo'].unique()
